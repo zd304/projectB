16db79639276c64a6f976e0c82027f28e0e55010
zd304 <568892758@qq.com>
2017-11-10 18:17:17 +0800

    1

---
+++ Assets/GameApp.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameApp : MonoBehaviour
+{
+	void Awake()
+	{
+		float fov = (float)Screen.width / (float)Screen.height;
+		float fHeight = mainCamera.orthographicSize;
+		float fWidth = mainCamera.orthographicSize * fov;
+
+		int col = 10;
+		int raw = 10;
+		float brickSize = 256.0f;
+
+		float gap = (float)Screen.width * 0.02f;
+		float w = ((float)Screen.width - (gap * (col + 1))) / col;
+		float scale = w / brickSize;
+
+		mBrickInst = Resources.Load<GameObject>("Prefab/brick_front");
+
+	}
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		
+	}
+
+	public Camera mainCamera;
+	GameObject mBrickInst = null;
+}
